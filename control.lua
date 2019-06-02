e36313f2ce102f15a72ad8befe64a01730317daf
Lucas Brandstaetter <lucas@brandstaetter.tech>
2019-06-02 18:26:46 +0200

    Remove dump debug function

--- control.lua
+++ control.lua
@@ -1013,18 +1013,3 @@ Event.register(
 )
 
 Gui.on_click("upgrade_blueprint", upgrade_blueprint)
-
-
-
-function dump(o)
-  if type(o) == 'table' then
-     local s = '{ '
-     for k,v in pairs(o) do
-        if type(k) ~= 'number' then k = '"'..k..'"' end
-        s = s .. '['..k..'] = ' .. dump(v) .. ','
-     end
-     return s .. '} '
-  else
-     return tostring(o)
-  end
-end
