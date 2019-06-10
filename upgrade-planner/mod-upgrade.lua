d0ba1114fd0277a65c17b4ff20ad7f0c5c785615
Lucas Brandstaetter <lucas@brandstaetter.tech>
2019-06-11 01:02:20 +0200

    Fix version typo

--- upgrade-planner/mod-upgrade.lua
+++ upgrade-planner/mod-upgrade.lua
@@ -1,8 +1,8 @@
 local UPGui = require("upgrade-planner/gui")
 
 return {
-  ["1.6.9"] = function()
-    game.print("Upgrade to version 1.6.9 of Upgrade Planner")
+  ["1.7.0"] = function()
+    game.print("Upgrade to version 1.7.0 of Upgrade Planner")
     global.current_config = {}
     for player_name, config in pairs(global.config) do
       global.current_config[game.players[player_name].index] = config
