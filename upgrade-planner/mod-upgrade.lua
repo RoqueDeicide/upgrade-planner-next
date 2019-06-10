0c7f9e2a0bb66507dddd3757f662810bf014bddb
Lucas Brandstaetter <lucas@brandstaetter.tech>
2019-06-10 22:58:25 +0200

    Remove unnecessary test

--- upgrade-planner/mod-upgrade.lua
+++ upgrade-planner/mod-upgrade.lua
@@ -2,11 +2,9 @@ return {
   ["1.6.9"] = function()
     game.print("Upgrade to version 1.6.9 of Upgrade Planner")
     global.current_config = {}
-    if global.config then
-      for player_name, config in pairs(global.config) do
-        global.current_config[game.players[player_name].index] = config
-      end
-      global.config = nil
+    for player_name, config in pairs(global.config) do
+      global.current_config[game.players[player_name].index] = config
     end
+    global.config = nil
   end
 }
