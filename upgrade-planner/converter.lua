436c255a3e3a806d1d6ae039c2561313198207a7
Lucas Brandstaetter <lucas@brandstaetter.tech>
2019-06-16 17:20:08 +0200

    Add module handling to converter

--- upgrade-planner/converter.lua
+++ upgrade-planner/converter.lua
@@ -63,14 +63,20 @@ end
 
 upgrade_planner_converter.to_upgrade_planner = function(stack, config, player)
   local entities = game.entity_prototypes
+  local items = game.item_prototypes
   stack.set_stack {name = "upgrade-planner"}
   local idx = 1
   for _, entry in pairs(config) do
     local entity_from = entities[entry.from]
     local entity_to = entities[entry.to]
+    local item_from = items[entry.from]
+    local item_to = items[entry.to]
     if entity_from and entity_from.fast_replaceable_group then
       stack.set_mapper(idx, "from", {type = "entity", name = entity_from.name})
+    elseif item_from and item_from.type == "module" then
+      stack.set_mapper(idx, "from", {type = "item", name = item_from.name})
     end
+
     if entity_to and entity_to.fast_replaceable_group then
       if entity_from then
         if entity_from.fast_replaceable_group == entity_to.fast_replaceable_group then
@@ -84,6 +90,8 @@ upgrade_planner_converter.to_upgrade_planner = function(stack, config, player)
       else
         stack.set_mapper(idx, "to", {type = "entity", name = entity_to.name})
       end
+    elseif item_to and item_to.type == "module" then
+      stack.set_mapper(idx, "to", {type = "item", name = item_to.name})
     end
     idx = idx + 1
   end
